f4a362d8349a8a0e38cb92c4ba4e798a2548e7f7
Samuel Shum <55974596+samshum90@users.noreply.github.com>
2020-11-18 00:46:05 +0000

    Add project files.

---
+++ StackOverFlow/Post.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOverFlow
+{
+    public class Post
+    {
+        private string title;
+        private string description;
+        private DateTime created;
+        private int upVotes;
+        private int downVotes;
+
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+        public DateTime Created { get => created; set => created = value; }
+        public int UpVotes { get => upVotes; set => upVotes = value; }
+        public int DownVotes { get => downVotes; set => downVotes = value; }
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(description))
+                throw new ArgumentNullException("Title and/or Description cannot be empty");
+            else
+                ShowVotes();
+        }
+        public void UpVote()
+        {
+            ++upVotes;
+            ShowVotes();
+        }        public void DownVote()
+        {
+            ++DownVotes;
+            ShowVotes();
+        }
+        public void ShowVotes()
+        {
+            Console.Clear();
+
+            Console.WriteLine();
+            Console.WriteLine("*-*-*-*-*-*-*-*-*");
+
+            Console.WriteLine("Title: " + Title);
+            Console.WriteLine("-");
+
+            Console.WriteLine("Description:");
+            Console.WriteLine(Description);
+
+            Console.WriteLine("-");
+            Console.WriteLine("Up-Votes:   " + upVotes);
+            Console.WriteLine("Down-Votes: " + downVotes);
+
+            Console.WriteLine("-");
+            Console.WriteLine("Date created: " + created);
+
+            Console.WriteLine();
+            Console.WriteLine("*-*-*-*-*-*-*-*-*");
+        }
+    }
+}
---
+++ StackOverFlow/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace StackOverFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                bool run = true;
+
+                Console.Write("Title of your post: ");
+                string postTitle = Console.ReadLine();
+
+                Console.Write("Description: ");
+                string postDescription = Console.ReadLine();
+
+                Post newPost = new Post(postTitle, postDescription);
+
+                while (run)
+                {
+                    Console.WriteLine("To up-vote this post press + (plus) or press - (minus) to down-vote this post.");
+                    Console.Write("Please enter your vote or press enter to quit: ");
+                    string userInput = Console.ReadLine();
+
+                    switch (userInput)
+                    {
+                        case "+":
+                            newPost.UpVote();
+                            continue;
+
+                        case "-":
+                            newPost.DownVote();
+                            continue;
+
+                        case "":
+                            newPost.ShowVotes();
+                            run = false;
+                            continue;
+
+                        default:
+                            newPost.ShowVotes();
+                            continue;
+                    }
+                }
+
+            }
+    }
+}
